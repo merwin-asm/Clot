5f8f8b71bf5f55d507a74d03e377acc705c82468
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-03-30 18:14:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,318 +18,329 @@ import requests
 
 ###################################
 
-def main():
+VERSION = "0.0.1"
+CLOT_DIR_ = __file__.split("/").pop()
+CLOT_DIR = ""
+for e in CLOT_DIR_:
+    CLOT_DIR += e
 
-    VERSION = "0.0.1"
-    CLOT_DIR_ = __file__.split("/").pop()
-    CLOT_DIR = ""
-    for e in CLOT_DIR_:
-        CLOT_DIR += e
+try:
+    conf =  open(CLOT_DIR + ".conf", "r")
+    COLOR = conf.read()
+    conf.close()
+except:
+    COLOR = ""
+
+if COLOR == "":
+    COLOR = False
+
+R = random.choice([70,100,150,180,240])
+G = random.choice([70,100,150,180,240])
+B = random.choice([70,100,150,180,240])
+
+OS = platform.system()
+
+
+"""
+def times_to():
+    size = os.get_terminal_size()
+    return int(size.columns)
+
+def back_space():
+    print("\b \b", end="")
+
+CUR_KEY = ""
+
+def on_press(key):
+    global CUR_KEY
+
+    if key == keyboard.Key.up:
+        for e in range(0, int(times_to())-10):
+            back_space()
+
+        cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
+
+        if CUR_KEY == "":
+            CUR_KEY = 1
+
+        else:
+            CUR_KEY += 1
+
+        print(bg(142, 155, 250)+fg(230,230,230)+f"@{os.getcwd()} [Root:{root}]"+fg.rs+bg.rs, end = "")
+        cmd = input(": ")
+        CUR_KEY = 1
+        print(cur[CUR_KEY * -1], end="")
+
+    elif key == keyboard.Key.down:
+        for e in range(0, int(times_to())):
+            back_space()
+
+        cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
+
+        if CUR_KEY == "":
+            CUR_KEY = 1
+
+        else:
+            CUR_KEY -= 1
+
+        CUR_KEY = 1
+        print(bg(142, 155, 250)+fg(230,230,230)+f"@{os.getcwd()} [Root:{root}]"+fg.rs+bg.rs, end = "")
+        cmd = input(": ")
+
+        print(cur[CUR_KEY * -1], end="")
+
+
+listener = keyboard.Listener(
+    on_press=on_press)
+listener.start()
+"""
+
+
+def add_to_history(cmd):
+    try:
+        cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
+    except:
+        cur = [""]
+        os.system(f"touch {CLOT_DIR}.history_terminal_s")
 
     try:
-        conf =  open(CLOT_DIR + ".conf", "r")
-        COLOR = conf.read()
-        conf.close()
+        if cur[-1][:-1] == cmd:
+            return None
     except:
-        COLOR = ""
+        pass
 
-    if COLOR == "":
-        COLOR = False
-
-
-    def times_to():
-        size = os.get_terminal_size()
-        return int(size.columns)
-
-    def back_space():
-        print("\b \b", end="")
-
-    CUR_KEY = ""
-
-    """
-    def on_press(key):
-        global CUR_KEY
-
-        if key == keyboard.Key.up:
-            for e in range(0, int(times_to())-10):
-                back_space()
-
-            cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
-
-            if CUR_KEY == "":
-                CUR_KEY = 1
-
-            else:
-                CUR_KEY += 1
-
-            print(bg(142, 155, 250)+fg(230,230,230)+f"@{os.getcwd()} [Root:{root}]"+fg.rs+bg.rs, end = "")
-            cmd = input(": ")
-            CUR_KEY = 1
-            print(cur[CUR_KEY * -1], end="")
-
-        elif key == keyboard.Key.down:
-            for e in range(0, int(times_to())):
-                back_space()
-
-            cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
-
-            if CUR_KEY == "":
-                CUR_KEY = 1
-
-            else:
-                CUR_KEY -= 1
-
-            CUR_KEY = 1
-            print(bg(142, 155, 250)+fg(230,230,230)+f"@{os.getcwd()} [Root:{root}]"+fg.rs+bg.rs, end = "")
-            cmd = input(": ")
-
-            print(cur[CUR_KEY * -1], end="")
+    f = open(CLOT_DIR+".history_terminal_s", "a")
+    f.write(cmd+"\n")
+    f.close()
 
 
-    listener = keyboard.Listener(
-        on_press=on_press)
-    listener.start()
-    """
-
-    def add_to_history(cmd):
-        try:
-            cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
-        except:
-            cur = [""]
-            os.system(f"touch {CLOT_DIR}.history_terminal_s")
-
-        try:
-            if cur[-1][:-1] == cmd:
-                return None
-        except:
-            pass
-
-        f = open(CLOT_DIR+".history_terminal_s", "a")
-        f.write(cmd+"\n")
-        f.close()
+def terminal_width():
+    return os.get_terminal_size().columns
 
 
+def colored(char):
+    global R, G, B
 
-    def terminal_width():
-        return os.get_terminal_size().columns
+    if R < 240:
+        R += 5
+        return fg(R, G, B) + char + fg.rs
 
-    R = random.choice([70,100,150,180,240])
-    G = random.choice([70,100,150,180,240])
-    B = random.choice([70,100,150,180,240])
+    if R == 240 and B < 240:
+        B += 5
+        return fg(R, G, B) + char + fg.rs
 
-    def colored(char):
-        global R, G, B
+    if R == 240 and B == 240 and G < 240:
+        G += 5
+        return fg(R, G, B) + char + fg.rs
 
-        if R < 240:
-            R += 5
-            return fg(R, G, B) + char + fg.rs
+    if R and  B and  G == 240:
+        R =  random.choice([70,100,150,180,240])
+        G =  random.choice([70,100,150,180,240])
+        B =  random.choice([70,100,150,180,240])
 
-        if R == 240 and B < 240:
-            B += 5
-            return fg(R, G, B) + char + fg.rs
+    if R < 240:
+        R += 5
+        return fg(R, G, B) + char + fg.rs
 
-        if R == 240 and B == 240 and G < 240:
-            G += 5
-            return fg(R, G, B) + char + fg.rs
+    if R == 240 and B < 240:
+        B += 5
+        return fg(R, G, B) + char + fg.rs
 
-        if R and  B and  G == 240:
-            R =  random.choice([70,100,150,180,240])
-            G =  random.choice([70,100,150,180,240])
-            B =  random.choice([70,100,150,180,240])
-
-        if R < 240:
-            R += 5
-            return fg(R, G, B) + char + fg.rs
-
-        if R == 240 and B < 240:
-            B += 5
-            return fg(R, G, B) + char + fg.rs
-
-        if R == 240 and B == 240 and G < 240:
-            G += 5
-            return fg(R, G, B) + char + fg.rs
+    if R == 240 and B == 240 and G < 240:
+        G += 5
+        return fg(R, G, B) + char + fg.rs
 
 
-    def exit_man():
-        try:
-            std_out.close()
-        except:
-            pass
-        try:
-            std_out_r.close()
-        except:
-            pass
-        try:
-            os.remove(CLOT_DIR+".<<std:out>>")
-        except:
-            pass
-        print("exited")
+def exit_man():
+    try:
+        std_out.close()
+    except:
+        pass
+    try:
+        std_out_r.close()
+    except:
+        pass
+    try:
+        os.remove(CLOT_DIR+".<<std:out>>")
+    except:
+        pass
+    print("exited")
 
-    def make_list(str_):
-        str_ = str_.replace(" ", "")
-        str_ = str_.split(",")
-        list_ = []
-        for e in str_:
-            list_.append(e)
-        return list_
+    
+def make_list(str_):
+    str_ = str_.replace(" ", "")
+    str_ = str_.split(",")
+    list_ = []
+    for e in str_:
+        list_.append(e)
+    return list_
 
-    def get_command_book(p = True):
-        global COLOR
 
-        file_ = CLOT_DIR + "cmd.book.db.json"
+def get_command_book(p = True):
+    global COLOR
 
-        try:
-            file = open(file_, "r")
-            content = json.loads(file.read())
-            file.close()
-        except:
-            content = ""
+    file_ = CLOT_DIR + "cmd.book.db.json"
 
-        if p:
+    try:
+        file = open(file_, "r")
+        content = json.loads(file.read())
+        file.close()
+    except:
+        content = ""
 
-            if content == "":
-                print("No Commands Stored")
-            else:
-                con = ""
-                n = 0
+    if p:
 
-                for e in content:
-                    con += f"{n} : {e} -- > {content[e]} \n"
-                    n += 1
-
-                for line in con:
-                    if  COLOR == False:
-                        for char in line:
-                            print(colored(char), end="")
-                    else:
-                        if type(COLOR) == str:
-                            COLOR = make_list(COLOR)
-                        print(fg(COLOR[0], COLOR[1], COLOR[2]) +line+ fg.rs, end="")
-
+        if content == "":
+            print("No Commands Stored")
         else:
-            if content == "":
-                return {}
+            con = ""
+            n = 0
 
-            return content
+            for e in content:
+                con += f"{n} : {e} -- > {content[e]} \n"
+                n += 1
 
-    def remove_command(cmd):
-        file_ = CLOT_DIR + "cmd.book.db.json"
+            for line in con:
+                if  COLOR == False:
+                    for char in line:
+                        print(colored(char), end="")
+                else:
+                    if type(COLOR) == str:
+                        COLOR = make_list(COLOR)
+                    print(fg(COLOR[0], COLOR[1], COLOR[2]) +line+ fg.rs, end="")
 
-        try:
-            file = open(file_, "r")
-            content = json.loads(file.read())
-            file.close()
+    else:
+        if content == "":
+            return {}
 
-            file = open(file_, "w")
-            del content[cmd]
-            file.write(json.dumps(content))
-            file.close()
+        return content
 
-        except:
-            pass
+    
+def remove_command(cmd):
+    file_ = CLOT_DIR + "cmd.book.db.json"
 
+    try:
+        file = open(file_, "r")
+        content = json.loads(file.read())
+        file.close()
 
-    def add_command(cmd, value):
-        file_ = CLOT_DIR + "cmd.book.db.json"
+        file = open(file_, "w")
+        del content[cmd]
+        file.write(json.dumps(content))
+        file.close()
 
-        try:
-            file = open(file_, "r")
-            content = json.loads(file.read())
-            file.close()
-
-            file = open(file_, "w")
-            content[cmd] = value
-            file.write(json.dumps(content))
-            file.close()
-
-        except:
-            file = open(file_, "w")
-            content = {}
-            content[cmd] = value
-            file.write(json.dumps(content))
-            file.close()
+    except:
+        pass
 
 
+def add_command(cmd, value):
+    file_ = CLOT_DIR + "cmd.book.db.json"
 
-    def search_command(keyword):
-        global COLOR
+    try:
+        file = open(file_, "r")
+        content = json.loads(file.read())
+        file.close()
 
-        content = get_command_book(p=False)
+        file = open(file_, "w")
+        content[cmd] = value
+        file.write(json.dumps(content))
+        file.close()
 
-        c = 0
-
-        for e in content:
-            if keyword in e or keyword in content[e]:
-
-                e_ = e
-
-                e = e.split(keyword)
-
-                c = 0
-                for x in e:
+    except:
+        file = open(file_, "w")
+        content = {}
+        content[cmd] = value
+        file.write(json.dumps(content))
+        file.close()
 
 
+def search_command(keyword):
+    global COLOR
 
-                    if  COLOR == False:
-                        for char in x:
-                            print(colored(char), end="")
+    content = get_command_book(p=False)
 
-                    else:
+    c = 0
 
-                        if type(COLOR) == str:
-                            COLOR = make_list(COLOR)
+    for e in content:
+        if keyword in e or keyword in content[e]:
 
-                        print(fg(COLOR[0], COLOR[1], COLOR[2]) + x + fg.rs, end="")
+            e_ = e
 
-                    if c != len(e) - 1:
+            e = e.split(keyword)
 
-                        print("\033[1;4m" + keyword + "\033[0m", end="")
+            c = 0
+            for x in e:
 
-                    c += 1
 
-                e = content[e_]
-                e = e.split(keyword)
 
-                print("  --->  ", end = "")
+                if  COLOR == False:
+                    for char in x:
+                        print(colored(char), end="")
 
-                c = 0
+                else:
 
-                for x in e:
+                    if type(COLOR) == str:
+                        COLOR = make_list(COLOR)
 
-                    if  COLOR == False:
-                        for char in x:
-                            print(colored(char), end="")
-                    else:
-                        if type(COLOR) == str:
-                            COLOR = make_list(COLOR)
+                    print(fg(COLOR[0], COLOR[1], COLOR[2]) + x + fg.rs, end="")
 
-                        print(fg(COLOR[0], COLOR[1], COLOR[2]) + x + fg.rs, end="")
+                if c != len(e) - 1:
 
-                    if c != len(e) - 1:
-                        print("\033[1;4m" + keyword + "\033[0m", end="") 
+                    print("\033[1;4m" + keyword + "\033[0m", end="")
 
-                    c +=  1
-                print("")
-                c = 0
+                c += 1
 
-    def is_root_linux():
-        return os.geteuid() == 0
+            e = content[e_]
+            e = e.split(keyword)
 
-    OS = platform.system()
+            print("  --->  ", end = "")
 
-    def clear():
-        if OS == "Windows":
-            os.system("cls")
-        else:
-            os.system("clear")
+            c = 0
 
-    def update(v):
-        os.system("pip3 uninstall clot-terminal")
-        os.system(f"pip3 install clot-terminal=={v}")
-        clear()
+            for x in e:
 
+                if  COLOR == False:
+                    for char in x:
+                        print(colored(char), end="")
+                else:
+                    if type(COLOR) == str:
+                        COLOR = make_list(COLOR)
+
+                    print(fg(COLOR[0], COLOR[1], COLOR[2]) + x + fg.rs, end="")
+
+                if c != len(e) - 1:
+                    print("\033[1;4m" + keyword + "\033[0m", end="") 
+
+                c +=  1
+            print("")
+            c = 0
+
+            
+def is_root_linux():
+    return os.geteuid() == 0
+
+OS = platform.system()
+
+def clear():
+    if OS == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+    
+def update(v):
+    os.system("pip3 uninstall clot-terminal")
+    os.system(f"pip3 install clot-terminal=={v}")
     clear()
 
+
+    
+def main():
+    global COLOR, CLOT_DIR, VERSION
+
+    VERSION = "0.0.1"
+
+    clear()
 
     std_out = ""
     std_out_r = ""
@@ -346,7 +357,7 @@ def main():
         try:
             print(bg(142, 155, 250)+fg(230,230,230)+f"@{os.getcwd()} [Root:{root}]"+fg.rs+bg.rs, end = "")
             cmd = input(": ")
-            CUR_KEY = ""
+
         except:
             pass
 
@@ -416,9 +427,12 @@ def main():
             continue
         if cmd == "update":
             version_ = requests.get("https://raw.githubusercontent.com/merwin-asm/Clot/main/current.ver").text
+            version_ = version_.replace("\n", "")
+            
             if version_ != VERSION:
                 update(version_)
             continue
+
         if cmd == "history":
             cur = open(CLOT_DIR+".history_terminal_s", "r").readlines()
             for e in cur:
@@ -514,6 +528,6 @@ def main():
         std_out_r.close()
 
         os.remove(CLOT_DIR+".<<std:out>>")
+        
 if __name__ == "__main__":
     main()
-        
